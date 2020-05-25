1f95439bc43988f7ee081076f749a9ecfc99c458
Enderson Maia <endersonmaia@gmail.com>
2020-05-25 18:44:06 -0300

    move leitos-AAAA-MM-DD-HHh.csv para ./leitos/AAAA-MM/AAAA-MM-DD/

--- download-microdados.sh
+++ download-microdados.sh
@@ -1,13 +1,16 @@
 #!/usr/bin/env sh
 
-SINTOMAS_CSV_FILE="sintomas-$(date +%Y-%m-%d-%H:%M:%S.%s)"
-SINTOMAS_DOWNLOAD_PATH=./sintomas
+SINTOMAS_CSV_FILE="sintomas-$(date +%Y-%m-%d-%H)h"
+SINTOMAS_BASE_PATH="./sintomas"
+SINTOMAS_DOWNLOAD_PATH="$SINTOMAS_BASE_PATH/$(date +%Y-%m)/$(date +%Y-%m-%d)"
+
+mkdir -p ${SINTOMAS_DOWNLOAD_PATH}
 
 echo "Baixando ultimo arquivo CSV de sintomas..."
 curl -fsSL -o "${SINTOMAS_DOWNLOAD_PATH}/${SINTOMAS_CSV_FILE}.csv" http://covid19.dados.al.gov.br/dados/sintomas
 if [ $? -eq "0" ]; then
     echo "Removendo duplicidades..."
-    fdupes --delete --noprompt ${SINTOMAS_DOWNLOAD_PATH}
+    fdupes --recurse --delete --noprompt ${SINTOMAS_BASE_PATH}
     echo "Feito."
 else
     echo "Falha ao baixar CSV"
