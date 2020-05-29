91a2bbeed572468b464795ca25ba47896146890d
Enderson Tadeu S. Maia <endersonmaia@gmail.com>
2020-05-29 08:31:32 -0300

    refactored download scripts

--- download-boletins.sh
+++ download-boletins.sh
@@ -1,43 +1,35 @@
-#!/usr/bin/env sh
+#!/bin/bash
 
-BOLETINS_HTML_FILE=boletins.html
-BOLETINS_DOWNLOAD_PATH=./boletins
+function get_pdfs() {
+    url=$1; shift
+    path=$1
 
-LEITOS_HTML_FILE=leitos.html
-LEITOS_DOWNLOAD_PATH=./leitos
+    HTML=arquivo.html
 
-echo "Recuperando lista de PDFs para baixar..."
-curl -sSL -o ${LEITOS_HTML_FILE} http://www.saude.al.gov.br/leitos-para-enfrentamento-da-covid-19/
-curl -sSL -o ${BOLETINS_HTML_FILE} http://cidadao.saude.al.gov.br/saude-para-voce/coronavirus/
-echo "Feito."
+    curl -sSL -o $HTML $url
+    echo "Recuperando lista de PDFs ..."
+    for pdf_url in $(hxnormalize $HTML | hxwls 2> /dev/null | grep -i '.pdf' | grep -i -e 'leito' -e 'informe'); do
+        pdf_filename=$(echo "$pdf_url" | sed 's/.*\///')
 
-echo "Baixando PDFs dos boletins..."
-for pdf in $(hxnormalize ${BOLETINS_HTML_FILE} | hxwls 2> /dev/null | grep -i pdf | grep -i informe); do
-    filename=$(echo "${pdf}" | sed 's/.*\///')
-    year_month_path=$(pdfinfo -isodates "${pdf}" | sed -e '/ModDate/!d;s/.*\([0-9]\{4\}\)-\([0-9]\{2\}\).*/\1-\2/')
-    filepath="${BOLETINS_DOWNLOAD_PATH}/${year_month_path}/${filename}"
-    mkdir -p "${BOLETINS_DOWNLOAD_PATH}/${year_month_path}"
+        find $path -name $pdf_filename > /dev/null
+        if [ $? -eq 0 ]; then
+            echo "$pdf_filename já baixado."
+            continue
+        fi
 
-    [ -f "$filepath" ] && echo "${filepath} já baixado." && continue
-    echo "Salvando em ${filepath} ..."
-    (cd ${BOLETINS_DOWNLOAD_PATH}/$year_month_path && curl -sSLO ${pdf})
-done
-rm ${BOLETINS_HTML_FILE}
-echo "Feito."
+        echo "Baixando $pdf_url ..."
+        (cd $path && curl -sSLO $pdf_url)
 
-echo "Baixando PDFs dos leitos..."
-for pdf in $(hxnormalize ${LEITOS_HTML_FILE} | hxwls 2> /dev/null | grep -i 'pdf'); do
-    filename=$(echo "${pdf}" | sed 's/.*\///')
-    year_month_path=$(pdfinfo -isodates "${pdf}" | sed -e '/ModDate/!d;s/.*\([0-9]\{4\}\)-\([0-9]\{2\}\).*/\1-\2/')
-    filepath="${LEITOS_DOWNLOAD_PATH}/${year_month_path}/${filename}"
-    mkdir -p "${LEITOS_DOWNLOAD_PATH}/${year_month_path}"
+        YYYY_MM=$(pdfinfo -isodates "$path/$pdf_filename" | sed -e '/ModDate/!d;s/.*\([0-9]\{4\}\)-\([0-9]\{2\}\).*/\1-\2/')
+        mkdir -p "$path/$YYYY_MM"
 
-    [ -f "${filepath}" ] && echo "${filepath} já baixado." && continue
-    echo "Salvando em ${filepath} ..."
-    (cd ${LEITOS_DOWNLOAD_PATH}/${year_month_path} && curl -sSLO ${pdf})
-done
+        mv $path/$pdf_filename $path/$YYYY_MM/$pdf_filename
+    done
 
-rm "${LEITOS_HTML_FILE}"
-rm "${BOLETINS_HTML_FILE}"
+    rm $HTML
 
-echo "Feito."
+    echo "Feito."
+}
+
+get_pdfs "http://www.saude.al.gov.br/leitos-para-enfrentamento-da-covid-19" "./leitos"
+get_pdfs "http://cidadao.saude.al.gov.br/saude-para-voce/coronavirus/" "./boletins"
