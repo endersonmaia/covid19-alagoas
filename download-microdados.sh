56372e68112618b248d92604d8ea5b9f7f554741
Enderson Tadeu S. Maia <endersonmaia@gmail.com>
2020-06-05 21:25:11 -0300

    Update download-microdados.sh
    update link do CSV

--- download-microdados.sh
+++ download-microdados.sh
@@ -7,7 +7,7 @@ SINTOMAS_DOWNLOAD_PATH="$SINTOMAS_BASE_PATH/$(date +%Y-%m)/$(date +%Y-%m-%d)"
 mkdir -p ${SINTOMAS_DOWNLOAD_PATH}
 
 echo "Baixando ultimo arquivo CSV de sintomas..."
-curl -fsSL -o "${SINTOMAS_DOWNLOAD_PATH}/${SINTOMAS_CSV_FILE}.csv" http://covid19.dados.al.gov.br/dados/sintomas
+curl -fsSL -o "${SINTOMAS_DOWNLOAD_PATH}/${SINTOMAS_CSV_FILE}.csv" https://covid19.dados.al.gov.br/sintomas
 if [ $? -eq "0" ]; then
     echo "Removendo duplicidades..."
     fdupes --recurse --delete --noprompt ${SINTOMAS_BASE_PATH}
